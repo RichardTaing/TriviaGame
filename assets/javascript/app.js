1fabd1d436bc931fbfde1bdae47e6269835e7545
rst_979 <richard.taing@gmail.com>
2019-07-15 23:46:15 +1000

    created html and added psuedo code into js

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+// ![Advanced]
+
+// ToDo: Pick a theme & Create Questions.
+// ToDo: Display one question at a time.
+// ToDo: 30 second Timer for every question.
+
+// Scenario: IF Player selects correct answer.
+//  -   display congratulations.
+//  -   5 second timmer.
+//  -   show next question.
+
+// Scenario: IF Player runs out of time.
+//  -   display times up.
+//  -   display correct answer.
+//  -   Show next question.
+
+// Scenario: IF Player chooses wrong answer.
+//  -   display to player they have incorrect answer.
+//  -   display correct answer.
+//  -   5 second timmer.
+//  -   Show next question.
+
+// ToDo: Final Screen - display number of correct answers.
+// ToDo: Final Screen - display number of incorrect answers.
+// ToDo: Final Screen - add restart button (NO reloading page).
