a51e37c303fcd4e4e740083e10fedb86e3060c57
rst_979 <richard.taing@gmail.com>
2019-07-20 18:09:49 +1000

    updated all

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,28 +26,28 @@ var TriviaObj = {
   showAnswerTime: 10000, // In milliseconds.
 
   questionsArr: [
-    "Who was the first player in NBA history to be elected league MVP by a unanimous vote?", // 1
-    "Who was the youngest player to score 10,000 points in the NBA?", // 2
-    "What player won All-Star Game MVP, NBA MVP, and NBA Finals MVP awards in 2000?", // 3
-    "Who won the Nestle Crunch Slam Dunk Contest on February 6, 1988?", // 4
-    "Why did Kevin Durant choose the jersey number 35?", // 5
-    "Who is the highest-scoring foreign-born player in NBA history?", // 6
-    "Who was the first player to win consecutive NBA MVP awards since Michael Jordan did it in 1991 and 1992?", // 7
-    "In 2015, which NBA player broke the record for most points scored in a quarter?", // 8
-    "In what year were the first Air Jordan sneakers released?", // 9
-    "Which one of these is not a 1st overall draft pick?", // 10
-    "Who was the first WNBA player to dunk in a playoff game?" // 11
+    "Who was the first player in NBA history to be elected league MVP by a unanimous vote?", // Q1
+    "Who was the youngest player to score 10,000 points in the NBA?", // Q2
+    "What player won All-Star Game MVP, NBA MVP, and NBA Finals MVP awards in 2000?", // Q3
+    "Who won the Nestle Crunch Slam Dunk Contest on February 6, 1988?", // Q4
+    "Why did Kevin Durant choose the jersey number 35?", // Q5
+    "Who is the highest-scoring foreign-born player in NBA history?", // Q6
+    "Who was the first player to win consecutive NBA MVP awards since Michael Jordan did it in 1991 and 1992?", // Q7
+    "In 2015, which NBA player broke the record for most points scored in a quarter?", // Q8
+    "In what year were the first Air Jordan sneakers released?", // Q9
+    "Which one of these is not a 1st overall draft pick?", // Q10
+    "Who was the first WNBA player to dunk in a playoff game?" // Q11
   ],
 
   answersObj: [
     [
-      "STEPHEN CURRY", // This one.
+      "STEPHEN CURRY", // Answer Q1.
       "MICHAEL JORDAN",
       "LEBRON JAMES",
       "MAGIC JOHNSON"
     ],
     [
-      "LEBRON JAMES", // This one.
+      "LEBRON JAMES", // Answer Q2.
       "WILT CHAMBERLAIN",
       "MICHAEL JORDAN",
       "KOBE BRYANT"
@@ -55,41 +55,41 @@ var TriviaObj = {
     [
       "TIM DUNCAN",
       "MICHAEL JORDAN",
-      "SHAQUILLE O'NEAL", // This one.
+      "SHAQUILLE O'NEAL", // Answer Q3.
       "KOBE BRYANT"
     ],
     [
       "DOMINIQUE WILKINS",
       "SPUD WEBB",
-      "MICHAEL JORDAN", // This one.
+      "MICHAEL JORDAN", // Answer Q4.
       "CLYDE DREXLER"
     ],
     [
       "He plans to play until age 35",
       "His hero was Reggie Lewis",
-      "His first coach was murdered at 35", // This one.
+      "His first coach was murdered at 35", // Answer Q5.
       "It's his first Tetrahedral Number"
     ],
     [
       "YAO MING",
       "HAKEEM OLAJUWON",
       "DETLEF SCHREMPF",
-      "DIRK NOWITZKI" // This one.
+      "DIRK NOWITZKI" // Answer Q6.
     ],
     [
       "LEBRON JAMES",
       "STEVE NASH",
-      "TIM DUNCAN", // This one.
+      "TIM DUNCAN", // Answer Q7.
       "STEPHEN CURRY"
     ],
     [
       "KEVIN DURANT",
       "KEVIN LOVE",
       "CARMELO ANTHONY",
-      "KLAY THOMPSON" // This one.
+      "KLAY THOMPSON" // Answer Q8.
     ],
     [
-      "1984", // This one.
+      "1984", // Answer Q9.
       "1987",
       "1991",
       "1993"
@@ -98,12 +98,12 @@ var TriviaObj = {
       "CHRIS WEBBER",
       "ALLEN IVERSON",
       "KENYON MARTIN",
-      "DIKEMBE MUTOMBO" // This one.
+      "DIKEMBE MUTOMBO" // Answer Q10.
     ],
     [
       "TAMIKA CATCHNGS",
       "LISA LESLIE",
-      "BRITTNEY GRINER", // This one.
+      "BRITTNEY GRINER", // Answer Q11.
       "MICHELLE SNOW"
     ]
   ],
@@ -166,15 +166,13 @@ var TriviaObj = {
           );
         });
         $(".triviaText").append("<div id='timer'></div>");
-
         this.questionTimer = 0;
-
         this.clockTimer = setInterval(this.timeCounter, 1000);
       } else if (this.totalTime == 0) {
         $(".triviaText").html(
           "<h3><b>Was that a buzzer beater OR a Brain Fade!!!</b></h3><br><br><br>" +
             "<button id='start' class='btn btn-secondary'> " +
-            "Press to Reload</button></div>" // Restarts game
+            "Press to Reload</button></div>" // Creates Reolad Button
         );
         $(".oneAnswer").empty();
         this.questionOn = false;
@@ -208,6 +206,7 @@ var TriviaObj = {
       TriviaObj.selectedAnswer = "noAnswer";
       TriviaObj.analyzeAnswer();
     }
+    // console.log(TriviaObj.questionTimer);
   },
 
   stopCounter() {
@@ -221,7 +220,6 @@ var TriviaObj = {
         "<h4><b>Time's up!!!</b></h4><br><br>" +
           "<h5>Look's like you need to polish up on your basketball knowledge.</h5>"
       );
-      //   $(".oneAnswer").css("background-color", "rgb(194, 194, 149)");
       setTimeout(function() {
         TriviaObj.questionOn = true;
         TriviaObj.showQuestion();
@@ -234,7 +232,6 @@ var TriviaObj = {
           this.rightAnswerArr[this.questionNumber] +
           "</h5>"
       );
-      //   $(".oneAnswer").css("background-color", "rgb(194, 194, 149)");
       this.giphyDisplay();
       this.rightAnswer++;
     } else {
@@ -243,7 +240,6 @@ var TriviaObj = {
           this.rightAnswerArr[this.questionNumber] +
           "<br><br><b>Sorry. Better Luck Next Time!</b></h5>"
       );
-      //   $(".oneAnswer").css("background-color", "rgb(194, 194, 149)");
       var gameSound = new Audio("assets/sounds/Buzzer-sports-arena.mp3");
       gameSound.volume = 0.55;
       gameSound.play();
@@ -350,6 +346,3 @@ $(document).on("click", "#start", function() {
 // Calling function to start the game =============================
 
 TriviaObj.startGame();
-
-// Todo: Questions loading before giphy has finished
-// Todo: Fix count. Make it count down not up
